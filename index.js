0671c14afeb41bac90ae4f29280a0abb4bcbf49f
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-28 22:56:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const numberOfDrumButtons = document.querySelectorAll(".drum").length
+
+for (let i = 0; i < numberOfDrumButtons; i ++) {
+    let eachButton = document.querySelectorAll(".drum")[i]
+    eachButton.addEventListener("click", function() {
+        
+    })
+}
