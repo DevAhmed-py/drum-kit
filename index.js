14488e0ec4f88621955ec25fcee496399ae24780
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-30 22:50:42 +0100

    commit

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const numberOfDrumButtons = document.querySelectorAll(".drum").length
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
-for (let i = 0; i < numberOfDrumButtons; i ++) {
-    let eachButton = document.querySelectorAll(".drum")[i]
+for (var i = 0; i < numberOfDrumButtons; i ++) {
+    var eachButton = document.querySelectorAll(".drum")[i]
     eachButton.addEventListener("click", function() {
 
-        let buttonInnerHTML = this.InnerHTML
+        var buttonInnerHTML = this.InnerHTML
 
         switch (buttonInnerHTML) {
             case "w":
-                const tom1 = new Audio("sounds/tom-1.mp3")
+                var tom1 = new Audio("sounds/tom-1.mp3")
                 tom1.play()
                 break;
             
             case "a":
-                const tom2 = new Audio("sounds/tom-2.mp3")
+                var tom2 = new Audio("sounds/tom-2.mp3")
                 tom2.play()
                 break;
 
             case "s":
-                const tom3 = new Audio("sounds/tom-3.mp3")
+                var tom3 = new Audio("sounds/tom-3.mp3")
                 tom3.play()
                 break;
 
             case "d":
-                const tom4 = new Audio("sounds/tom-4.mp3")
+                var tom4 = new Audio("sounds/tom-4.mp3")
                 tom4.play()
                 break;
 
